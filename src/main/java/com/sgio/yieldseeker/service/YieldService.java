1091651f27aaa7b67c408c36940e7a1b6b19b843
alfsgio coface <sa_apptech@outlook.fr>
2024-05-24 17:08:21 +0200

    24/05 - Bienici with jsoup first try

--- src/main/java/com/sgio/yieldseeker/service/YieldService.java
+++ src/main/java/com/sgio/yieldseeker/service/YieldService.java
@@ -28,24 +28,37 @@ public class YieldService {
     public String startScrapper() {
 //        String apiUrl = "https://www.seloger.com/search-bff/api/externaldata?from=0&size=25";
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
 //        headers.set("Cookie","AB-TESTING=%7B%22relatedAdsPosition%22%3A%22relatedAdsAfterDocumentationServices%22%7D; i18next=fr; webGLBenchmarkScore=9.964354697102722");
 //        headers.set("Referer","https://www.bienici.com/recherche/achat/chilly-mazarin-91380/appartement/studio?surface-min=30&classification-energetique=A%2CB%2CC%2CD");
-        headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0");
+//        headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0");
+//
+//        String apiTest = "https://www.bienici.com/realEstateAds.json?filters=%7B%22size%22%3A24%2C%22from%22%3A0%2C%22showAllModels%22%3Afalse%2C%22filterType%22%3A%22buy%22%2C%22propertyType%22%3A%5B%22flat%22%5D%2C%22maxRooms%22%3A1%2C%22minArea%22%3A30%2C%22energyClassification%22%3A%5B%22A%22%2C%22B%22%2C%22C%22%2C%22D%22%5D%2C%22page%22%3A1%2C%22sortBy%22%3A%22relevance%22%2C%22sortOrder%22%3A%22desc%22%2C%22onTheMarket%22%3A%5Btrue%5D%2C%22mapMode%22%3A%22enabled%22%2C%22limit%22%3A%22wvihHcx%7DL%3Fs%7BGl%60CZ%3F%7CyG%22%2C%22newProperty%22%3Afalse%2C%22blurInfoType%22%3A%5B%22disk%22%2C%22exact%22%5D%2C%22zoneIdsByTypes%22%3A%7B%22zoneIds%22%3A%5B%22-395443%22%5D%7D%7D&extensionType=extendedIfNoResult";
+//        String filters = URLEncoder.encode("{\"size\":24,\"from\":0,\"showAllModels\":false,\"filterType\":\"buy\",\"propertyType\":[\"flat\"],\"maxRooms\":1,\"minArea\":30,\"energyClassification\":[\"A\",\"B\",\"C\",\"D\"],\"page\":1,\"sortBy\":\"relevance\",\"sortOrder\":\"desc\",\"onTheMarket\":[true],\"mapMode\":\"enabled\",\"limit\":\"wvihHcx}L?s{Gl`CZ?|yG\",\"newProperty\":false,\"blurInfoType\":[\"disk\",\"exact\"],\"zoneIdsByTypes\":{\"zoneIds\":[\"-395443\"]}}&extensionType=extendedIfNoResult", StandardCharsets.UTF_8);
+//        String apiTestTest = "https://www.bienici.com/realEstateAds.json?filters={filters}";
+//        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(headers);
+//        ResponseEntity<String> responseEntity = restTemplate.exchange(apiTestTest, HttpMethod.GET, requestEntity, String.class);
+//
+//        return responseEntity.getBody();
 
-        String apiTest = "https://www.bienici.com/realEstateAds.json?filters=%7B%22size%22%3A24%2C%22from%22%3A0%2C%22showAllModels%22%3Afalse%2C%22filterType%22%3A%22buy%22%2C%22propertyType%22%3A%5B%22flat%22%5D%2C%22maxRooms%22%3A1%2C%22minArea%22%3A30%2C%22energyClassification%22%3A%5B%22A%22%2C%22B%22%2C%22C%22%2C%22D%22%5D%2C%22page%22%3A1%2C%22sortBy%22%3A%22relevance%22%2C%22sortOrder%22%3A%22desc%22%2C%22onTheMarket%22%3A%5Btrue%5D%2C%22mapMode%22%3A%22enabled%22%2C%22limit%22%3A%22wvihHcx%7DL%3Fs%7BGl%60CZ%3F%7CyG%22%2C%22newProperty%22%3Afalse%2C%22blurInfoType%22%3A%5B%22disk%22%2C%22exact%22%5D%2C%22zoneIdsByTypes%22%3A%7B%22zoneIds%22%3A%5B%22-395443%22%5D%7D%7D&extensionType=extendedIfNoResult";
-        String filters = URLEncoder.encode("{\"size\":24,\"from\":0,\"showAllModels\":false,\"filterType\":\"buy\",\"propertyType\":[\"flat\"],\"maxRooms\":1,\"minArea\":30,\"energyClassification\":[\"A\",\"B\",\"C\",\"D\"],\"page\":1,\"sortBy\":\"relevance\",\"sortOrder\":\"desc\",\"onTheMarket\":[true],\"mapMode\":\"enabled\",\"limit\":\"wvihHcx}L?s{Gl`CZ?|yG\",\"newProperty\":false,\"blurInfoType\":[\"disk\",\"exact\"],\"zoneIdsByTypes\":{\"zoneIds\":[\"-395443\"]}}&extensionType=extendedIfNoResult", StandardCharsets.UTF_8);
-        String apiTestTest = "https://www.bienici.com/realEstateAds.json?filters={filters}";
-        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(apiTestTest, HttpMethod.GET, requestEntity, String.class);
-
-        return responseEntity.getBody();
+        return scrapIt();
     }
 
-//    private String scrapIt() {
-//
-//    }
+    private String scrapIt() {
+        try {
+            Document document = Jsoup.connect("https://www.bienici.com/recherche/achat/chilly-mazarin-91380/appartement/studio?surface-min=25&classification-energetique=A%2CB%2CC%2CD&page=NaN").get();
+            Elements elements = document.getElementsByClass(".detailedSheetLink");
+
+            for(Element e : elements){
+                System.out.println(e.attr("href"));
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return "";
+    }
 
     private Map<String, Object> getParameters(){
         Map<String, Object> parameters = new HashMap<>();
